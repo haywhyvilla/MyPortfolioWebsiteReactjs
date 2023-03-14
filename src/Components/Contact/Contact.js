59d90436c4265f05487deb911462d2d0dda4cda7
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-03-14 11:13:50 +0100

    socials

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Contact.css";
 import { RiSendPlaneLine } from "react-icons/ri";
+import { FaLinkedin, FaFacebook, FaTwitter, FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   return (
@@ -32,7 +35,41 @@ const Contact = () => {
             </form>
           </div>
         </div>
-        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5"></div>
+        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 connect">
+          <div className="text">
+            <h2>Let's talk about everything web and mobile.</h2>
+            <h5>
+              If you have any questions about any project, don't hesitate to
+              make contact.
+            </h5>
+            <h5>
+              I'm available to grab a snack and chat! Drop a comment, question,
+              or a concern by E-mail or WhatsApp.
+            </h5>
+            <a href="mailto:olatunjiayobami575@gmail.com">
+              <AiOutlineMail />
+              olatunjiayobami575@gmail.com
+            </a>
+            <a href="https://wa.me/+2347017521808">
+              <BsWhatsapp />
+              https://wa.me/+2347017521808
+            </a>
+          </div>
+          <div className="socials">
+            <a href="https://www.linkedin.com/in/olatunji-ayobami-853300207/">
+              <FaLinkedin />
+            </a>
+            <a href="https://web.facebook.com/profile.php?id=100073473931605">
+              <FaFacebook />
+            </a>
+            <a href="https://twitter.com/haywhyvilla">
+              <FaTwitter />
+            </a>
+            <a href="https://github.com/haywhyvilla">
+              <FaGithub />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
