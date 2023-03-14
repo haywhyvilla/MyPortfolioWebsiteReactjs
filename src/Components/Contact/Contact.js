e2d8f1feaf236c398081521ef0a466fb2f35c03b
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-03-14 09:30:59 +0100

    contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Contact.css";
+import { RiSendPlaneLine } from "react-icons/ri";
 
 const Contact = () => {
   return (
@@ -18,6 +19,15 @@ const Contact = () => {
               <div className="contact-form">
                 <label className="form-label">E-mail</label>
                 <input type="text" className="form-control" />
+              </div>
+              <div className="contact-form">
+                <label className="form-label">Message</label>
+                <textarea rows="4" type="text" className="form-control" />
+              </div>
+              <div className="button-submit">
+                <p>
+                  Send <RiSendPlaneLine />
+                </p>
               </div>
             </form>
           </div>
