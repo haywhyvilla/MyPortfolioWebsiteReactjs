eb98344e1671d409546a141858fd512d7f8e3f12
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-03-20 01:37:44 +0100

    lets go

--- src/Components/Projects/ProjectSite.js
+++ src/Components/Projects/ProjectSite.js
@@ -1,28 +1,31 @@
 import React from "react";
 import "./Projects.css";
 import { FiExternalLink } from "react-icons/fi";
+import { Zoom } from "react-reveal";
 
 const ProjectSite = ({ Des, ProjectLink, GithubLink, image }) => {
   return (
-    <div className="project-site">
-      <div className="projects">
-        <div>
-          <p>{Des}</p>
-        </div>
-        <div>
-          <img src={image} alt="just-me" />
-        </div>
+    <Zoom>
+      <div className="project-site">
+        <div className="projects">
+          <div>
+            <p>{Des}</p>
+          </div>
+          <div>
+            <img src={image} alt="just-me" />
+          </div>
 
-        <div>
-          <a href={ProjectLink}>
-            Live Site <FiExternalLink />
-          </a>
-          <a target="_" href={GithubLink}>
-            GitHub Link <FiExternalLink />
-          </a>
+          <div>
+            <a href={ProjectLink}>
+              Live Site <FiExternalLink />
+            </a>
+            <a target="_" href={GithubLink}>
+              GitHub Link <FiExternalLink />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </Zoom>
   );
 };
 
